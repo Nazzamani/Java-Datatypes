64bceac50a0c66fb10edc9f97024ecd70d9b8286
Nazzamani <it_zamani@yahoo.com>
2019-04-18 18:53:38 +0430

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,7 +3,6 @@ import java.io.*;
 /* When an integer is signed, one of its bits becomes the sign bit, meaning that the maximum magnitude
 of the number is halved. (So an unsigned 32-bit int can store up to 2^32-1, whereas its signed counterpart
 has a maximum positive value of 2^31-1.) */
-
 //In Java, all integer types are signed (except char).
 
 class Solution{
